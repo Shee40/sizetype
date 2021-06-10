7e8678a3181bb8be64fd88e1f4156d8ed45bedd5
Shee40 <83650794+Shee40@users.noreply.github.com>
2021-06-10 19:14:42 +0530

    Create sizetype.cc

---
+++ sizetype.cc
@@ -0,0 +1,32 @@
+//c++ program to find the size of int,float,char,double.
+#include<iostream>
+using namespace std;
+int main()
+{
+  int integerType;
+  char charType;
+  float floatType;
+  double doubleType;
+  cout<<"size of int is:"<<sizeof(integerType)<<"/n";
+  cout<<"size of char is:"<<sizeof(chatType)<<"/n";
+  cout<<"size of float is:"<<sizeof(floatType)<<"/n";
+  cout<<"size of double is:"<<sizeof(doubleType)<<"/n";
+  return 0;
+}
+
+
+//c++ program to find the product of two numbers.
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,product;
+  cout<<"enter two numbers\n";
+  cin >>a >>b;
+  product =a*b;
+  cout<<"product ="<<product;
+  return 0;
+}
+  
+
+
